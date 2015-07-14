1fdc6e698f2b83e0949f08f38099613eebe726cd
baranbartu <bbartu.demirci@gmail.com>
2015-07-14 14:40:33 +0300

    index name was changed

--- location_tracker_djrest/settings.py
+++ location_tracker_djrest/settings.py
@@ -137,6 +137,6 @@ HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
         'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'events',
+        'INDEX_NAME': 'haystack_djrest',
     },
 }
