9a8bd834fcc68b8459dfabda863d9cb3149ada3e
baranbartu <bbartu.demirci@gmail.com>
2015-07-19 19:06:13 +0300

    jmeter test preparation

--- app/management/commands/cleaninstall.py
+++ app/management/commands/cleaninstall.py
@@ -1,16 +1,14 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
-from app.management.commands.droptables import Command as dtcommand
-
-
 __author__ = 'baran'
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        dtcommand().handle(args, options)
+        call_command('droptables', **options)
         call_command('syncdb', interactive=False)
+        call_command('rebuild_index', **options)
 
 
 
